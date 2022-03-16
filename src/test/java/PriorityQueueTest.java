8e9785d675298b5a462915fc1a1a531682912989
redchchicken75 <tanhongjie.cs07@nctu.edu.tw>
2022-03-17 00:09:52 +0800

    Similate failed environment

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PriorityQueueTest {
     static Stream<Arguments> streamProvider() {
         return Stream.of(
-                Arguments.of(new int[]{5, 4, 2, 3}, new int[]{2, 3, 4, 5}),
+                Arguments.of(new int[]{5, 4, 2, 3}, new int[]{2, 3, 4}),
                 Arguments.of(new int[]{10, 100000, 1000, 0, 1}, new int[]{0, 1, 10, 1000, 100000}),
                 Arguments.of(new int[]{336, 214, Integer.MAX_VALUE, Integer.MIN_VALUE}, new int[]{ Integer.MIN_VALUE, 214, 336, Integer.MAX_VALUE}),
                 Arguments.of(new int[]{0, 0, 0}, new int[]{0, 0, 0}),
