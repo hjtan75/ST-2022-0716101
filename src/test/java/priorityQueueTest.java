bcc37db8847333a85ddf1686119c62f583f5666b
redchchicken75 <tanhongjie.cs07@nctu.edu.tw>
2022-03-16 22:48:29 +0800

    second commit

--- src/test/java/priorityQueueTest.java
+++ src/test/java/priorityQueueTest.java
@@ -1,18 +1,15 @@
+package java;
 import com.sun.jdi.connect.Connector;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.stream.Stream;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class priorityQueueTest {
+public class PriorityQueueTest {
     static Stream<Arguments> streamProvider() {
         return Stream.of(
                 Arguments.of(new int[]{5, 4, 2, 3}, new int[]{2, 3, 4, 5}),
@@ -55,7 +52,7 @@ class priorityQueueTest {
             PriorityQueue<Integer> test = new PriorityQueue<Integer>(-1);
         });
     }
-//
+    //
     @Test
     public void whenExceptionThrown_thenNoElementCanRemove() {
         Exception exception = assertThrows(NullPointerException.class, () -> {
